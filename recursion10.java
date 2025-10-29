e9c3c96e18004f296d7744d912db42852097622a
Ch Harshita <harshitachavva9@gmail.com>
2025-10-29 16:48:16 +0530

    Day 10: Recursion (leetcode #119)

---
+++ recursion10.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+class recursion10 {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        long val = 1;
+        for (int i = 0; i <= rowIndex; i++) {
+            row.add((int)val);
+            val = val * (rowIndex - i) / (i + 1);
+        }
+        return row;
+    }
+}
+
